bf49074010ef363c6614b5165421fcb9415cde70
berto <162624084+00berto@users.noreply.github.com>
2025-03-31 22:36:38 +0200

    dict y1,y2,y3

--- src/js/main.js
+++ src/js/main.js
@@ -362,11 +362,11 @@ function generateChart(data) {
     valoresX_grafico2,
     valoresY1_grafico2,
     valoresY2_grafico2,
-    valoresY3_grafico2,
+    valoresY3_grafico2 /*
     "\nValores de los Dict\n",
     dictY1_grafico2,
     dictY2_grafico2,
-    dictY3_grafico2
+    dictY3_grafico2*/
   );
 
   //min max
